ed477edc2aed7453becc05f0dc9a46bd9305a7f5
aksharakoppula <aadya.koppula@gmail.com>
2025-10-14 19:57:16 -0400

    timer.py

--- timer.py
+++ timer.py
@@ -1,16 +1,46 @@
 import tkinter as tk
 class Timer:
     def __init__ (self, root):
+        self.minutes=45
+        self.seconds=0
         self.root=root
         self.root.title("Timer")
-        self.label = tk.Label(root, text='Time:')
+        self.label = tk.Label(root, text=f"{self.minutes:02d}:{self.seconds:02d}")
         self.label.pack(pady=20)
-        self.button = tk.Button(root, text='Start', bg="pink")
+        self.button = tk.Button(root, text='Start', command=self.start_timer, bg="pink")
         self.button.pack(pady=20)
+        self.stopbutton = tk.Button(root, text='Stop', bg="lightblue")
+        self.stopbutton.pack(pady=20)
+        self.pausebutton = tk.Button(root, text='Pause', bg="lightyellow")
+        self.pausebutton.pack(pady=20)
+        self.running=False
+    def start_timer (self):
+        if not self.running:
+            self.running=True
+            self.countdown()
+    def countdown (self):
+        if self.running:
+            if self.seconds == 0:
+                if self.minutes > 0:
+                    self.minutes -= 1
+                    self.seconds = 59
+            else:
+                self.running = False
+                self.label.config(text="Time's Up!")
+                return
+        else:
+            self.seconds -= 1
+
+        self.label.config(text=f"{self.minutes:02d}:{self.seconds:02d}")
+        self.root.after(1000, self.countdown)
+
+
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     pomodoro_timer = Timer(root)
     root.mainloop()
+    
+
 
