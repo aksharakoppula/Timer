6b74cd8c2d54d36ac5d58ca30e436940e4561a66
aksharakoppula <aadya.koppula@gmail.com>
2025-10-21 19:31:10 -0400

    timer.py

--- timer.py
+++ timer.py
@@ -9,10 +9,10 @@ class Timer:
         self.label.pack(pady=20)
         self.button = tk.Button(root, text='Start', command=self.start_timer, bg="pink")
         self.button.pack(pady=20)
-        self.stopbutton = tk.Button(root, text='Stop', bg="lightblue")
+        self.stopbutton = tk.Button(root, text='Stop', command=self.end, bg="lightblue")
         self.stopbutton.pack(pady=20)
-        self.pausebutton = tk.Button(root, text='Pause', bg="lightyellow")
-        self.pausebutton.pack(pady=20)
+        self.resetbutton = tk.Button(root, text='Reset', command=self.reset, bg="lightyellow")
+        self.resetbutton.pack(pady=20)
         self.running=False
     def start_timer (self):
         if not self.running:
@@ -24,23 +24,26 @@ class Timer:
                 if self.minutes > 0:
                     self.minutes -= 1
                     self.seconds = 59
+                else:
+                    self.running = False
+                    self.label.config(text="Time's Up!")
+                    return
             else:
-                self.running = False
-                self.label.config(text="Time's Up!")
-                return
-        else:
-            self.seconds -= 1
+                self.seconds -= 1
 
+            self.label.config(text=f"{self.minutes:02d}:{self.seconds:02d}")
+            self.root.after(1000, self.countdown)
+    def end (self):
+        if self.running:
+            self.running=False
+    def reset (self):
+        self.running=False
+        self.seconds=0
+        self.minutes=45
         self.label.config(text=f"{self.minutes:02d}:{self.seconds:02d}")
-        self.root.after(1000, self.countdown)
-
-
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     pomodoro_timer = Timer(root)
     root.mainloop()
-    
-
-
